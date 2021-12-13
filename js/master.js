3db8593ce81c522e8e931a9a1873278140cc9838
ErikKnops <94675426+ErikKnops@users.noreply.github.com>
2021-12-13 10:59:52 +0100

    responsiveness update

---
+++ js/master.js
@@ -0,0 +1,11 @@
+function show() {
+  document.getElementById('burger').style.display = 'none';
+  document.getElementById('balkhead').style.display = 'block';
+  document.getElementById('sluit').style.display = "block";
+}
+
+function hide() {
+  document.getElementById('burger').style.display = 'block';
+  document.getElementById('balkhead').style.display = 'none';
+  document.getElementById('sluit').style.display = "none";
+}
